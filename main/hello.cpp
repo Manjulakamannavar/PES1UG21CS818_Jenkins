01a46f622f95f10545872ae6f78ca400220f0baa
Manjulakamannavar <110068391+Manjulakamannavar@users.noreply.github.com>
2023-02-19 12:06:18 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, Manjula_PES1UG21CS818\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
